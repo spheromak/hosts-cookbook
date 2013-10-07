2d51e981f5f96aebb4ec7570fb8eb37eb52ad39b
Juozas Gaigalas <j.gaigalas@accenture.com>
2013-10-07 16:26:45 +0100

    default hosts file attribute for windows

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,6 @@
-default[:hosts][:config] = "/etc/hosts"
+case node[:platform_family]
+when 'windows'
+  default[:hosts][:config] = "#{ENV['SystemRoot']}\\system32\\drivers\\etc\\hosts"
+else
+  default[:hosts][:config] = "/etc/hosts"
+end
